393a49598340d0e2230894921599add6f70eb67e
Laszlo Daru <daruu@hotmail.com>
2021-01-08 11:02:05 +0100

    feat(Flie structure)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ArticlePage from './components/ArticlePage/ArticlePage';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Hello World!!!</h1>
+			<ArticlePage />
 		</div>
 	);
 }
---
+++ src/components/ArticlePage/ArticlePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ArticlePage.css';
+
+const ArticlePage = () => {
+	return (
+		<div className='ArticlePage'>
+			<div className='ArticlePage_article'>
+				Of course, not everything in your report will exactly be you, there are
+				so many factors that make you, you. We found the best way to approach
+				your Gravity report is to use it as a guide to understand your
+				predispositions. The report can give you an idea about your underlying
+				approaches to life. That can be powerful stuff! People have said to us
+				things like “it is cathartic to know these traits are pre-determined!”
+				Or now I understand why I’m always doing x y z. We hope you enjoy the
+				journey and take something from these reports. We know we did.
+			</div>
+		</div>
+	);
+};
+
+export default ArticlePage;
